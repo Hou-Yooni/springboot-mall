2f5a09a1cddca7e80a36d898ab56f10e5ac381a8
Yooni Hou <mooiao@yahoo.com.tw>
2022-09-22 23:54:21 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/yoonihou/springbootmall/controller/ProductController.java
+++ src/main/java/com/yoonihou/springbootmall/controller/ProductController.java
@@ -52,4 +52,11 @@ public class ProductController {
         Product updateProduct = productService.getProductById(productId);
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+        productService.deleteProduct(productId);
+
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
