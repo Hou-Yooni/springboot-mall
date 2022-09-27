d4eefec6da143b9f5ed67ec14f151fbd5bd3264c
Yooni Hou <mooiao@yahoo.com.tw>
2022-09-27 23:01:59 +0800

    帳號功能 - 檢查註冊的 email | 隱藏返回的密碼

--- src/main/java/com/yoonihou/springbootmall/dao/UserDao.java
+++ src/main/java/com/yoonihou/springbootmall/dao/UserDao.java
@@ -5,5 +5,7 @@ import com.yoonihou.springbootmall.model.User;
 
 public interface UserDao {
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
--- src/main/java/com/yoonihou/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/yoonihou/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.yoonihou.springbootmall.dao.UserDao;
 import com.yoonihou.springbootmall.dto.UserRegisterRequest;
 import com.yoonihou.springbootmall.model.User;
 import com.yoonihou.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private  final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +25,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        // 檢查註冊的 email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        if(user != null){
+            throw new RuntimeException(String.valueOf(HttpStatus.BAD_REQUEST));
+        }
+        log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
